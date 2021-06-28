76a5067856584a0eced205490752d94e4a0b5060
Etienne Filliat <etienne.filliat@epitech.eu>
2021-06-28 14:11:34 +0200

    Add better Dock support

--- config/bspwm/scripts/multimonitor.sh
+++ config/bspwm/scripts/multimonitor.sh
@@ -3,12 +3,13 @@
 # set up the two monitors for bspwm
 # NOTE This is a simplistic approach because I already know the settings I
 # want to apply.
-external_monitor=$(xrandr --query | grep 'DP-1')
+external_monitor=$(xrandr --query | grep -w 'DP-1')
 if [[ $external_monitor = *disconnected* ]]; then
     external_monitor=$(xrandr --query | grep -w 'DVI-I-2-1')
 fi
 
 if [[ ! $external_monitor = *disconnected* ]]; then
+    external_monitor=$(echo $external_monitor | cut -d" " -f1)
     echo "Monitor $external_monitor connected"
     xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output $external_monitor --mode 1920x1080 --rotate normal --right-of eDP-1
 else
