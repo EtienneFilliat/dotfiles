4bd53151f25ce251575e9bc9c4e510af8b93201f
Etienne Filliat <etienne.filliat@epitech.eu>
2021-06-28 17:57:24 +0200

    Update Scripts post firmware update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,14 +8,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # From https://github.com/polybar/polybar/issues/763#issuecomment-331604987
 # Querry ALL Available screens
+
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        [[ $m == 'eDP-1' ]] && bar='main' || bar='main_external'
+        echo "Monitor [$m] - Bar: [$bar]"
         DEFAULT_WIRED_NETWORK_INTERFACE=$(ip route | grep '^default.*enp.*$' | awk '{print $5}' | head -n1) \
         MONITOR=$m \
-            polybar -c ~/.config/polybar/config.ini main_external &
+            polybar -c ~/.config/polybar/config.ini $bar &
     done
-else
-    # Launch Polybar, using default config (~/.config/polybar/config)
-    DEFAULT_WIRED_NETWORK_INTERFACE=$(ip route | grep '^default.*enp.*$' | awk '{print $5}' | head -n1) \
-        polybar -c ~/.config/polybar/config.ini main &
 fi
